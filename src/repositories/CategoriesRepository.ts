a64d14079d11eb85f224f13c4eb1ae1ff72707f8
Wesleson22 <wesllesonsowza@gmail.com>
2022-11-12 10:40:51 -0300

    6. Criando repositório de categoria

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,28 @@
+
+import { Category } from "../model/Category";
+
+interface IcreateCategotyDTO{
+    name: string;
+    description: string;
+}
+class CategoriesRepository{
+   private categories: Category[];
+
+   constructor(){
+    this.categories=[];
+    
+   }
+//DTO => Data Transfer Object
+   create({name, description}: IcreateCategotyDTO): void{
+    const category = new Category();
+    Object.assign(category,{
+        name,
+        description,
+        created_at: new Date(),
+    }); 
+
+    this.categories.push(category);
+   }
+
+}
+export {CategoriesRepository}
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,21 +1,14 @@
 import {Router} from 'express';
 import {v4 as uuidV4} from 'uuid';
 import { Category } from '../model/Category';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (resquest, response) => {
     const {name, description} = resquest.body;
 
-    const category = new Category();
-    Object.assign(category,{
-        name,
-        description,
-        created_at: new Date(),
-    }); 
-
-    categories.push(category);
-
-    return response.status(201).json({category});
+    categoriesRepository.create({name,description});
+    return response.status(201).send();
 });
 export {categoriesRoutes};
