cd4cc7b2cd7018431373fb596dc0077080317a48
Wesleson22 <wesllesonsowza@gmail.com>
2022-11-12 10:53:27 -0300

    8. Validando o cadastro de categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -5,8 +5,14 @@ import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
+
 categoriesRoutes.post("/", (resquest, response) => {
     const {name, description} = resquest.body;
+    const categoryAlreadyExists = categoriesRepository.findByName(name);
+    if(categoryAlreadyExists){
+        return response.status(400).json({error: "Category Already exists!"});
+        
+    }
 
     categoriesRepository.create({name,description});
     return response.status(201).send();
