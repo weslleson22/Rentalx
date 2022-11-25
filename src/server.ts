74453721f5e859d3704b61ad2100ffccb590c97c
Wesleson22 <wesllesonsowza@gmail.com>
2022-11-25 20:36:52 -0300

    9. Refatorando as rotas

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import  express  from "express";
+import { router } from "./routes";
 import { categoriesRoutes } from "./routes/categories.routes";
 import { specificationRoutes } from "./routes/specifications.routes";
 
 const app = express();
 app.use(express.json());
 
-app.use("/categories",categoriesRoutes);
-app.use("/specifications",specificationRoutes);
+
+app.use(router)
 
 app.listen(3333, ()=>console.log("Server is running!!"));
