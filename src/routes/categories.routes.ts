5e1637c56956bf00e86b970b73128b024dfb791c
Wesleson22 <wesllesonsowza@gmail.com>
2022-11-30 20:32:38 -0300

    2. Criando upload de arquivos

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,4 +1,4 @@
-import {Router} from 'express';
+import {response, Router} from 'express';
 import {v4 as uuidV4} from 'uuid';
 import { Category } from '../modules/cars/model/Category';
 import { CategoriesRepository } from '../modules/cars/repositories/implementations/CategoriesRepository';
@@ -6,8 +6,13 @@ import { createCategoryController } from '../modules/cars/useCases/CreateCategor
 //import { PostgresCaregoriesRepository } from '../modules/cars/repositories/PostgresCategoriesRepository';
 import { CreateCategoryUseCase } from '../modules/cars/useCases/CreateCategory/CreateCategoryUseCase';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
+import multer  from 'multer';
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+    dest:"./tmp",
+});
 const categoriesRepository = CategoriesRepository.getIntance();
 
 categoriesRoutes.post("/", (resquest, response) => {
@@ -17,5 +22,12 @@ categoriesRoutes.post("/", (resquest, response) => {
 categoriesRoutes.get("/", (request, response)=>{
     
     return listCategoriesController.handle(request, response);
+});
+
+categoriesRoutes.post("/import",upload.single("file"), (request, response)=>{
+    const {file} = request;
+    console.log(file);    
+    return response.send();
+
 });
 export {categoriesRoutes};
