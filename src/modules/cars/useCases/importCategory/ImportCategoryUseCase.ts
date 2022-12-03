ef0b1937add7f75b3b5e4682bc7bad63eaba48ad
Wesleson22 <wesllesonsowza@gmail.com>
2022-12-03 20:03:23 -0300

    7. Inserindo os dados do upload no repositório

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -41,6 +41,18 @@ class ImportCategoryUseCase{
         const categories= await this.loadCategories(file);
         console.log(categories);
 
+        categories.map(async category=>{
+            const {name, description}= category;
+
+            const existCategory =  this.categoriesRepository.findByName(name);
+
+            if(!existCategory){
+                this.categoriesRepository.create({
+                    name,description,
+                });
+            }
+        });
+
     }
 }
 export {ImportCategoryUseCase}
