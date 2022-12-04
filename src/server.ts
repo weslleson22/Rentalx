4e6f74401bb58927bc39c387d9f5130f7a2d9830
Wesleson22 <wesllesonsowza@gmail.com>
2022-12-03 21:02:15 -0300

    2. Configurando o swagger

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import  express  from "express";
 import { router } from "./routes";
-import { categoriesRoutes } from "./routes/categories.routes";
-import { specificationRoutes } from "./routes/specifications.routes";
+
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 app.use(express.json());
 
-
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router)
 
 app.listen(3333, ()=>console.log("Server is running!!"));
